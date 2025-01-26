5025e4e2b9e267064b3acbe306a44182f6a4472c
nebeltag <nebeltag@mail.ru>
2025-01-26 21:41:57 +0300

    update js 15

--- js/app.js
+++ js/app.js
@@ -2343,6 +2343,7 @@
         const basketSimplebarList = document.querySelector(".basket-list");
         const basketProductsList = indexPage && basketSimplebarList.querySelector(".simplebar-content");
         let productsData = [];
+        const mobileMenuOpen = document.documentElement.contains(".menu__open");
         indexPage && openBasketBtn.addEventListener("click", (function() {
             basket.classList.toggle("_show-cart");
             getBasketProducts();
@@ -2356,6 +2357,10 @@
             basket.querySelector(".error a").addEventListener("click", (function(e) {
                 e.preventDefault();
                 basket.classList.remove("_show-cart");
+                if (mobileMenuOpen) {
+                    hamburgerButton.setAttribute("data-state", "closed");
+                    hamburgerButton.setAttribute("aria-expanded", "false");
+                }
             }));
         }
         getBasketProducts();
